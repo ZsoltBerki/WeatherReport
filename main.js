5c4cd32d1e1a518fe1b7f0e67436720b2b3941a7
Zsolt Berki <berkizsolt90@gmail.com>
2019-09-12 19:14:45 +0300

    Add React, webpack, babel and eslint

--- main.js
+++ main.js
@@ -1,53 +1,35 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+//TODO move these to a config file.
+const MAIN_ENTRY_FILE = 'file://' + __dirname + '/app/index.html';
+const WINDOW_WIDTH = 800;
+const WINDOW_HEIGHT = 600;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const windowSettings = {
+  width: WINDOW_WIDTH,
+  height: WINDOW_HEIGHT
+};
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+const { app, BrowserWindow } = require('electron');
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+const debug = require('electron-debug');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+//TODO make this appear in dev mode only
+debug();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
+let mainWindow = null;
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('window-all-closed', function() {
+  console.log('All windows are closed, closing down the application.');
+  //if (process.platform != 'darwin') {
+  app.quit();
+  //}
+});
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on('ready', function() {
+  mainWindow = new BrowserWindow(windowSettings);
+  console.log('Loading from ' + MAIN_ENTRY_FILE);
+  mainWindow.loadURL(MAIN_ENTRY_FILE);
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
---
+++ src/main/react/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MainView from './views/main/MainView';
+
+export default () => {
+  return <MainView />;
+};
